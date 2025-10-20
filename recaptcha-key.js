04cc39b033ac33a6483a004f8cddb92576427247
VortexBypass <taykay1056@gmail.com>
2025-10-20 01:57:46 -0300

    Delete recaptcha-key.js

--- recaptcha-key.js
+++
@@ -1,9 +0,0 @@
-// api/recaptcha-key.js
-module.exports = async (req, res) => {
-  if (req.method !== 'GET') {
-    res.status(405).json({ error: 'Method not allowed. Use GET.' });
-    return;
-  }
-  const siteKey = process.env.RECAPTCHA_SITE_KEY || '';
-  res.status(200).json({ siteKey });
-};
